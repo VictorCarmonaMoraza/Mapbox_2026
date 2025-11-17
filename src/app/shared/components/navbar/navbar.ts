ac860405281ef2079b6e580066ff2d4584b3bb48
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-17 19:39:13 +0100

    Titulo en el Navbar - Observable -Señal

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { routes } from '../../../app.routes';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter, map, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [AsyncPipe, RouterLink],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
 
+  //Obtener el url de las rutas y asignar un título predeterminado si no existe
+  router = inject(Router);
+
+  // Filtrar las rutas para excluir la ruta comodín '**'
+  routes = routes.map(route => ({
+    path: route.path,
+    title: `${route.title ?? 'Maps en Angular 2026'}`
+  })).filter(route => route.path !== '**');
+
+  pageTitle = toSignal(this.router.events.pipe(
+    filter(event => event instanceof NavigationEnd),
+    // tap((event) => console.log(event)),
+    map(event => event.url),
+    map(url => routes.find((route) => `/${route.path}` === url)?.title ?? 'Mapas')
+  ));
 }
